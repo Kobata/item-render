799e3b81ce78efa890cba607eb78cf92f20bd8e1
Kobata <kobata2@gmail.com>
2013-11-13 18:01:36 -0500

    Render entities in isometric view.
    The bounds calculation needs improved still.

--- src/itemrender/client/KeybindRenderEntity.java
+++ src/itemrender/client/KeybindRenderEntity.java
@@ -10,6 +10,7 @@ import itemrender.client.rendering.FBOHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.Render;
@@ -81,7 +82,8 @@ public class KeybindRenderEntity extends KeyBindingRegistry.KeyHandler {
 
             GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-            GuiInventory.func_110423_a(0, 0, 1, 1, 1, current);
+            renderEntity(current);
+            //GuiInventory.func_110423_a(0, 0, 1, 1, 1, current);
 
             GL11.glMatrixMode(GL11.GL_PROJECTION);
             GL11.glPopMatrix();
@@ -93,6 +95,45 @@ public class KeybindRenderEntity extends KeyBindingRegistry.KeyHandler {
 
             fbo.restoreTexture();
         }
+    }
+
+    private void renderEntity(EntityLivingBase entity) {
+
+        GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+        GL11.glPushMatrix();
+        GL11.glScalef((float)(-1), (float)1, (float)1);
+        GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+        float f2 = entity.renderYawOffset;
+        float f3 = entity.rotationYaw;
+        float f4 = entity.rotationPitch;
+        float f5 = entity.prevRotationYawHead;
+        float f6 = entity.rotationYawHead;
+        GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
+
+        GL11.glRotatef((float)Math.toDegrees(Math.asin(Math.tan(Math.toRadians(30)))), 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(-45, 0.0F, 1.0F, 0.0F);
+
+        entity.renderYawOffset = (float)Math.atan((double)(1 / 40.0F)) * 20.0F;
+        entity.rotationYaw = (float)Math.atan((double)(1 / 40.0F)) * 40.0F;
+        entity.rotationPitch = -((float)Math.atan((double)(1 / 40.0F))) * 20.0F;
+        entity.rotationYawHead = entity.rotationYaw;
+        entity.prevRotationYawHead = entity.rotationYaw;
+        GL11.glTranslatef(0.0F, entity.yOffset, 0.0F);
+        RenderManager.instance.playerViewY = 180.0F;
+        RenderManager.instance.renderEntityWithPosYaw(entity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+        entity.renderYawOffset = f2;
+        entity.rotationYaw = f3;
+        entity.rotationPitch = f4;
+        entity.prevRotationYawHead = f5;
+        entity.rotationYawHead = f6;
+        GL11.glPopMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 
     @Override
