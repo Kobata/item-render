dad52293ec71720e2e73dd8ad472c1207e5428f4
Kobata <kobata2@gmail.com>
2013-11-13 04:40:29 -0500

    Added warning message if GL3.2 not detected.

--- src/itemrender/ItemRenderMod.java
+++ src/itemrender/ItemRenderMod.java
@@ -13,6 +13,8 @@ import itemrender.client.RenderTickHandler;
 import net.minecraftforge.common.Configuration;
 import org.lwjgl.opengl.GLContext;
 
+import java.util.logging.Logger;
+
 @Mod(modid = "@MODID@")
 public class ItemRenderMod {
     @Mod.Instance("@MODID@")
@@ -26,9 +28,13 @@ public class ItemRenderMod {
     private int mainTextureSize;
     private int gridTextureSize;
 
+    public Logger log;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
+        log = e.getModLog();
         gl32_enabled = GLContext.getCapabilities().OpenGL32;
+
         Configuration config = new Configuration(e.getSuggestedConfigurationFile());
         mainTextureSize = config.get(Configuration.CATEGORY_GENERAL, "mainTextureSize", DEFAULT_MAIN_TEXTURE_SIZE).getInt();
         gridTextureSize = config.get(Configuration.CATEGORY_GENERAL, "gridTextureSize", DEFAULT_GRID_TEXTURE_SIZE).getInt();
@@ -43,6 +49,8 @@ public class ItemRenderMod {
             KeyBindingRegistry.registerKeyBinding(defaultRender);
             KeyBindingRegistry.registerKeyBinding(new KeybindRenderInventoryBlock(gridTextureSize, "_grid"));
             KeyBindingRegistry.registerKeyBinding(new KeybindToggleRender());
+        } else {
+            log.severe("OpenGL 3.2 not detected, mod will not work!");
         }
     }
 
